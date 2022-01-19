fb6c3921e213e8a23327d93c3deff73a81f5c11d
Seamus <seamymc@live.co.uk>
2022-01-19 09:52:01 +0000

    minor tweak

--- app/Jobs/ModerateUploads.php
+++ app/Jobs/ModerateUploads.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Pint;
+use Illuminate\Support\Facades\Log;
 
 class ModerateUploads implements ShouldQueue
 {
@@ -35,7 +36,6 @@ class ModerateUploads implements ShouldQueue
      */
     public function handle()
     {
-        echo($pint->beer->name);
-        echo($pint->user->name);
+        Log::info("Adding to moderation queue: " . $pint->beer->name);
     }
 }
