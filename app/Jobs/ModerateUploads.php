0d5e9680b83d8dbe40b6a89ed5bc1401527e2795
Seamus <seamymc@live.co.uk>
2022-01-25 22:21:35 +0000

    Wednesday session

--- app/Jobs/ModerateUploads.php
+++ app/Jobs/ModerateUploads.php
@@ -10,6 +10,8 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Pint;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
 
 class ModerateUploads implements ShouldQueue
 {
@@ -36,6 +38,24 @@ class ModerateUploads implements ShouldQueue
      */
     public function handle()
     {
-        Log::info("Adding to moderation queue: " . $pint->beer->name);
+        Log::info("Adding to moderation queue: " . $this->pint->price);
+        //Some code to send text
+    
+        $api_key = 'H334DXcT6sOp2QLggxMYJU6XpwI5dTMkNGeQd7jaqzGiFR';
+
+        $response = Http::withHeaders([
+            'Authorization' => 'H334DXcT6sOp2QLggxMYJU6XpwI5dTMkNGeQd7jaqzGiFR'
+        ])->post('https://api.voodoosms.com/sendsms', [
+            'to' => 447799474101,
+            'from' => "VoodooSMS",
+            'msg' => "Hello User! t",
+            'sandbox' => true
+        ]);
+
+        
+        Log::info($response);
+
+
     }
+        
 }
